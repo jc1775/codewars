dd22e2e142b57eca23507e8024e8c726e7ae15d1
Joseph Calarco <jcalarco26@bell.net>
2020-01-24 11:10:50 -0500

    translatelite

--- translateLite.py
+++ translateLite.py
@@ -5,5 +5,13 @@ def translate_lite(sentence):
         new_string += word[-1::-1] + " "
     return new_string
 
-test = "This is a test string"
-print(translate_lite(test))
+
+def onethird(n):
+    sum = 0
+    for i in range(1,(n+1)):
+        sum += (i**2)
+    ans = sum / (n**3)
+    return ans
+
+test = 5
+print(onethird(test))
